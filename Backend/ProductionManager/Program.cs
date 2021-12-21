e263104696eaf3bf850723b79e98ea43890e039a
Guy Powell <guy@PinaVida.co.uk>
2021-12-21 09:42:01 -0600

    Error check on start if existing instance is running

--- Backend/ProductionManager/Program.cs
+++ Backend/ProductionManager/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Solarponics.ProductionManager.Domain;
+using Solarponics.ProductionManager.Factories;
+using System;
+using System.Diagnostics;
+using System.Linq;
 
 namespace Solarponics.ProductionManager
 {
@@ -9,6 +13,20 @@ namespace Solarponics.ProductionManager
         {
             try
             {
+                var proc = Process.GetCurrentProcess();
+                var procs = Process.GetProcesses();
+                if (procs.Any(p => p.Id != proc.Id && p.ProcessName == proc.ProcessName))
+                {
+                    var res = new DialogBox(new DialogBoxWindowFactory(), new DialogBoxWindowViewModelFactory()).Show("Production manager is already running.  Do you want to terminate the existing instance?  If you select Yes the existing production manager will be stopped a new instance will run.  If you select No the existing instance will remain.", buttons: Enums.DialogBoxButtons.YesNo);
+                    if (!res)
+                        return;
+
+                    var procsToTerminate = procs.Where(p => p.Id != proc.Id && p.ProcessName == proc.ProcessName);
+                    foreach (var procToTerminate in procsToTerminate)
+                        procToTerminate.Kill();
+
+                    System.Threading.Thread.Sleep(1000);
+                }
                 System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
                 var app = new App();
                 app.Run();
