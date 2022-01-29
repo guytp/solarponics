fd79c14d7b11e011abfe703b0878dba538aac240
Guy Powell <guy@PinaVida.co.uk>
2022-01-29 13:58:28 -0600

    Added more humanf riendlye rror handlign for hardwaer failures

--- Backend/ProductionManager/Hardware/HardwareProvider.cs
+++ Backend/ProductionManager/Hardware/HardwareProvider.cs
@@ -1,4 +1,5 @@
 ﻿using Solarponics.Models;
+using Solarponics.ProductionManager.Abstractions;
 using Solarponics.ProductionManager.Abstractions.ApiClients;
 using Solarponics.ProductionManager.Abstractions.Hardware;
 using System;
@@ -11,7 +12,8 @@ namespace Solarponics.ProductionManager.Hardware
         private HardwareSettings hardwareSettings;
         private readonly IDriverProvider driverProvider;
         private readonly IHardwareApiClient apiClient;
-        
+        private readonly IDialogBox dialogBox;
+
         public IBarcodeScanner BarcodeScanner { get; private set; }
 
         public IScale Scale { get; private set; }
@@ -19,36 +21,77 @@ namespace Solarponics.ProductionManager.Hardware
         public ILabelPrinter LabelPrinterSmall { get; private set; }
         public ILabelPrinter LabelPrinterLarge { get; private set; }
 
-        public HardwareProvider(IHardwareApiClient apiClient, IDriverProvider driverProvider)
+        public HardwareProvider(IHardwareApiClient apiClient, IDriverProvider driverProvider, IDialogBox dialogBox)
         {
             this.apiClient = apiClient;
             this.driverProvider = driverProvider;
+            this.dialogBox = dialogBox;
         }
 
         public async Task Start()
         {
             Stop();
-            this.hardwareSettings = await this.apiClient.GetSettings(Environment.MachineName) ?? new HardwareSettings();
+            try
+            {
+                this.hardwareSettings = await this.apiClient.GetSettings(Environment.MachineName) ?? new HardwareSettings();
+            }
+            catch (Exception ex)
+            {
+                this.dialogBox.Show("Failed to load hardare settings from backend.  No harwdare will be available.  Restart the app and if this continues contact support.", ex);
+                return;
+            }
             
             if (this.hardwareSettings.BarcodeScanner != null)
             {
-                this.BarcodeScanner = this.driverProvider.Get<IBarcodeScanner>(this.hardwareSettings.BarcodeScanner);
-                this.BarcodeScanner.Start();
+                try
+                { 
+                    this.BarcodeScanner = this.driverProvider.Get<IBarcodeScanner>  (this.hardwareSettings.BarcodeScanner);
+                    this.BarcodeScanner.Start();
+                }
+                catch (Exception ex)
+                {
+                    this.dialogBox.Show("Failed to start barcode scanner.  The barcode scanner won't be availblle.  Ensure it is turned on and connected then restart the app.  If this problem continues, please contact support.", ex);
+                    return;
+                }
             }
             if (this.hardwareSettings.Scale != null)
             {
-                this.Scale = this.driverProvider.Get<IScale>(this.hardwareSettings.Scale);
-                this.Scale.Start();
+                try
+                {
+                    this.Scale = this.driverProvider.Get<IScale>(this.hardwareSettings.Scale);
+                    this.Scale.Start();
+                }
+                catch (Exception ex)
+                {
+                    this.dialogBox.Show("Failed to start scales.  The scales won't be availblle.  Ensure they are turned on and connected then restart the app.  If this problem continues, please contact support.", ex);
+                    return;
+                }
             }
             if (this.hardwareSettings.LabelPrinterSmall != null)
             {
-                this.LabelPrinterSmall = this.driverProvider.Get<ILabelPrinter>(this.hardwareSettings.LabelPrinterSmall);
-                this.LabelPrinterSmall.Start();
+                try
+                { 
+                    this.LabelPrinterSmall = this.driverProvider.Get<ILabelPrinter>(this.hardwareSettings.LabelPrinterSmall);
+                    this.LabelPrinterSmall.Start();
+                }
+                catch (Exception ex)
+                {
+                    this.dialogBox.Show("Failed to start small label printer.  The printer won't be availblle.  Ensure it is turned on and connected then restart the app.  If this problem continues, please contact support.", ex);
+                    return;
+                }
             }
             if (this.hardwareSettings.LabelPrinterLarge != null)
             {
-                this.LabelPrinterLarge = this.driverProvider.Get<ILabelPrinter>(this.hardwareSettings.LabelPrinterLarge);
-                this.LabelPrinterLarge.Start();
+                try
+                { 
+                    this.LabelPrinterLarge = this.driverProvider.Get<ILabelPrinter>(this.hardwareSettings.LabelPrinterLarge);
+                    this.LabelPrinterLarge.Start();
+                }
+                catch (Exception ex)
+                {
+                    this.dialogBox.Show("Failed to start large label printer.  The printer won't be availblle.  Ensure it is turned on and connected then restart the app.  If this problem continues, please contact support.", ex);
+                    return;
+                }
             }
         }
 
